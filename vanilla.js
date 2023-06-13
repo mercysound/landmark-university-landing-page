ce6b54c210432f873617d2c36972916f7a3530ab
mercysound <adekunleadewolemercy@gmail.com>
2023-06-13 17:28:04 +0100

    perfect done

---
+++ vanilla.js
@@ -0,0 +1,15 @@
+const navdate = new Date;
+
+function displayTime(){
+  var dateTime = new Date()
+  var hrs = dateTime.getHours()
+  var min = dateTime.getMinutes()
+  var sec = dateTime.getSeconds()
+  var session = document.getElementById('sessiom');
+
+  document.getElementById("hours").innerHTML = hrs;
+  document.getElementById("minutes").innerHTML = min;
+  document.getElementById("seconds").innerHTML = sec;
+
+}
+setInterval(displayTime, 10);
